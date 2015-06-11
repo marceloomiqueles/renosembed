5ee39a2e8a9a76ec61b206bfca4fdb1b2cd972b4
Marcelo Miqueles <marcelomiqueles@hotmail.com>
2015-06-11 18:41:34 -0300

    Repositorio embed

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<iframe src="http://culturebox.francetvinfo.fr/resultats/widgets/external.html?source_type=festival&id=ms-56657&player=advanced&language=en&width=530&height=300&size=auto" width="550" height="780" onload="this.style.height = this.contentWindow.document.body.scrollHeight + 'px';" frameborder="0" scrolling="no"></iframe>
+	</body>
+</html>
